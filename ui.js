696fcf239290692009ccfdbe8fc939e0ad90583c
rickie120243 <rickie120243@gmail.com>
2014-07-20 03:37:51 +0800

    Idiom Search ver2
    data(1570)upload
    dump fix
    附上注音、漢語拼音

--- ui.js
+++ ui.js
@@ -1,6 +1,6 @@
 //front end
 var showidiom=function(idiom){
-	return "<b>"+idiom.key+"</b> : "+idiom.def;
+	return "<b>"+idiom.key+"</b> : "+"注音 : "+idiom.prounce+" 漢語拼音 : "+idiom.enspell+"<br>"+idiom.def;
 }
 var dosearch=function(tofind){
 	var arr=search(tofind);
